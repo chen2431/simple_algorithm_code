e7b9226200ca61268b9ab81a36ddf53c7d74ea32
Wanghongchen <chen2431@hotmail.com>
2015-12-03 11:18:52 +0800

    isprime add new function in isprime.py, this function may faster

--- isprime/isprime.py
+++ isprime/isprime.py
@@ -1,4 +1,5 @@
 #!/bin/python
+import math
 
 def isprime(n):
 	if n<2:
@@ -13,11 +14,31 @@ def isprime(n):
 				return False
 		return True
 
+def isprime_ex1(x):
+	if x<2: return False
+	if x==2:return True
+
+	s = math.sqrt(x)
+	m = int(s+1)
+	t = range(2, m)
+	#print t
+	for i in t:
+		if x%i==0:
+			return False
+
+	return True
+
+
 n = input("input an integer:")
-for i in range(1,n):
-	b = isprime(i)
-	if b:
-		print(i),
-		print(' is prime')
+a = range(2, n)
+o = filter(isprime_ex1, a)
+
+print o
+
+#for i in range(1,n):
+#	b = isprime_ex1(i)
+#	if b:
+#		print(i),
+#		print(' is prime')
 #	else:
 #		print(' is composite')
